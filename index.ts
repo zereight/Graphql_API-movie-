7a880f98a8de24cca6688dcd4f67cd03308607c4
zereight <aldwk68@naver.com>
2018-10-07 14:10:15 +0000

    4. Add Server middleware & Options

--- index.ts
+++ index.ts
@@ -1,14 +1,41 @@
-import {GraphQLServer} from "graphql-yoga";
+import {GraphQLServer,Options} from "graphql-yoga";
 
 import resolvers from "./graphql/resolvers";
 
-const server : GraphQLServer = new GraphQLServer({
-    typeDefs: "graphql/schema.graphql",
-    resolvers: resolvers
-});
+import cors from "cors";
+import logger from "morgan";
+import helmet from "helmet";
 
-const printStart = ():void => {
-    console.log("Server start!");
+class Server{
+    public server: GraphQLServer;
+    
+    public Option : Options = {
+    
+        port: process.env.PORT || 4000,
+        playground: "/playground",
+        endpoint: "/graphql"
+    };
+    
+    public server_Starter = ():void => {
+        this.server.express.use(cors());
+        this.server.express.use(helmet());
+        this.server.express.use(logger("start"));
+    };
+    
+    constructor(){
+        this.server = new GraphQLServer({
+            typeDefs: "graphql/schema.graphql",
+            resolvers: resolvers
+        });
+        this.server_Starter();
+    }
+    
+};
+
+const printUsingPORT = ():void => {
+    console.log(`Using PORT : ${newServer.Option.port}`);
 }
 
-server.start(printStart);
+const newServer = new Server();
+newServer.server.start(newServer.Option,printUsingPORT);
+
